360cd0bd81fd9b31d7c423b9945a77ef597aa256
yamadicu <dicutia@gmail.com>
2023-06-28 17:17:21 +0200

    creazione tech

---
+++ app/Http/Controllers/Admin/TechnologyController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Admin\Technology;
+use Illuminate\Http\Request;
+
+class TechnologyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Admin\Technology  $technology
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Technology $technology)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Admin\Technology  $technology
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Technology $technology)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Admin\Technology  $technology
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Technology $technology)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Admin\Technology  $technology
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Technology $technology)
+    {
+        //
+    }
+}
